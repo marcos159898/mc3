ec0bbe1291012dbfdc6a13a4b7f201f155fd5939
marcos159898 <130581896+marcos159898@users.noreply.github.com>
2024-06-17 10:52:35 -0300

    .

--- script.js
+++ script.js
@@ -20,19 +20,4 @@ const perguntas = [
         ]
     }
 ]; 
-const perguntas = [
-    {
-        enunciado: "Pergunta 1",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
-    },
-    {
-        enunciado: "Pergunta 2",
-        alternativas: [
-            "Alternativa 1",
-            "Alternativa 2"
-        ]
-    }
-]; 
+
